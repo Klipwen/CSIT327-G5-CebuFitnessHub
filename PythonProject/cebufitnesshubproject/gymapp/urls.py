6e2e66ac79932543201537a7b6592e7be5a8507e
sorasai <angel.cy1316@gmail.com>
2025-10-19 18:42:14 +0800

    fix(routing): Fix redirect issues after login and registration

--- PythonProject/cebufitnesshubproject/gymapp/urls.py
+++ PythonProject/cebufitnesshubproject/gymapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import landing_view, register_member, member_dashboard, staff_dashboard_view, logout_confirm_view, logout_prompt_view, staff_logout_view
+from .views import landing_view, register_member, member_login, staff_login, member_dashboard, staff_dashboard_view, logout_confirm_view, logout_prompt_view, staff_logout_view
 
 urlpatterns = [
     path('', landing_view, name='landing'),
     path('register/', register_member, name='register_submission'),
+    path('login/', member_login, name='member_login'),
+    path('staff-login/', staff_login, name='staff_login'),
     path('dashboard/', member_dashboard, name='member_dashboard'),
     path('staff_dashboard/', staff_dashboard_view, name='staff_dashboard'),
     path('logout/', logout_prompt_view, name='logout_prompt'),
