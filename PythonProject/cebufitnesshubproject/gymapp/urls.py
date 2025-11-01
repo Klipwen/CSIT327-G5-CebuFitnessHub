3dfd9ef280d03ea9d05e0dc0771735c71bfcac7d
Elvin Opalla Lagamo Jr. <elvin.lagamo@cit.edu>
2025-11-02 03:12:51 +0800

    feat: add billing history and check-in routes

--- PythonProject/cebufitnesshubproject/gymapp/urls.py
+++ PythonProject/cebufitnesshubproject/gymapp/urls.py
@@ -13,6 +13,8 @@ from .views import (
     request_account_unfreeze,
     request_account_freeze,
     member_details_view,
+    check_in_view,
+    billing_history_view,
 )
 
 urlpatterns = [
@@ -29,5 +31,7 @@ urlpatterns = [
     path('account/request-unfreeze/', request_account_unfreeze, name='request_account_unfreeze'),
     path('account/request-freeze/', request_account_freeze, name='request_account_freeze'),
     path('member_details/', member_details_view, name='member_details'),
+    path('check_in/', check_in_view, name='check_in'),
+    path('billing_history/', billing_history_view, name='billing_history'),
 ]
  
