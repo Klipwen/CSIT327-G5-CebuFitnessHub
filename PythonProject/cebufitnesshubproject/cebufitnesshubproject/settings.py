fc3132210f3a1b8d7e0c35dcc1fb2bac8e665b6d
Elvin Opalla Lagamo Jr. <elvin.lagamo@cit.edu>
2025-11-13 23:34:54 +0800

    chore(settings): Add 'humanize' to INSTALLED_APPS and set TIME_ZONE to 'Asia/Manila'

--- PythonProject/cebufitnesshubproject/cebufitnesshubproject/settings.py
+++ PythonProject/cebufitnesshubproject/cebufitnesshubproject/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'gymapp',
 ]
 
@@ -180,7 +181,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila' #Philippine time
 
 USE_I18N = True
 
